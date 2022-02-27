4a95189950683041bec88271818201fc72ba1ab7
ehsanansari <ansari.eh@gmail.com>
2022-02-27 12:59:42 -0600

    repository fo fetching and parsing photo Items.

---
+++ lib/gallery/repositories/photo_repository.dart
@@ -0,0 +1,35 @@
+import 'package:http/http.dart' as http;
+import '../models/models.dart';
+import 'dart:convert';
+
+
+class PhotoRepository {
+  static const baseUrl = "https://jsonplaceholder.typicode.com";
+  static const resourceType = "photos";
+  static const int querylimit = 25;
+
+  http.Client httpClient = http.Client();
+
+  /// Dependency injection for tests
+  PhotoRepository({http.Client? client}){
+    httpClient = client ?? http.Client();
+  }
+
+
+  Future<List<PhotoModel>> fetchPhotos({required int startIndex}) async {
+
+    final String _photoQuertUrl =
+        "$baseUrl/$resourceType?_start=$startIndex&_limit=$querylimit";
+    final _response = await httpClient.get(Uri.parse(_photoQuertUrl));
+
+    if (_response.statusCode == 200) {
+      final json = jsonDecode(_response.body);
+      List<PhotoModel> photosList =
+          (json as List).map((e) => PhotoModel.fromJson(e)).toList();
+ 
+      return photosList;
+    } else {
+      throw Exception("Something went wrong!");
+    }
+  }
+}
