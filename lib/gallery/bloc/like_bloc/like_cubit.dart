21b43242d36b4ba4db160d8504ed70dadc4a878c
ehsanansari <ansari.eh@gmail.com>
2022-02-27 15:25:03 -0600

    updated like cubit with new methods for registering likes and unlikes

--- lib/gallery/bloc/like_bloc/like_cubit.dart
+++ lib/gallery/bloc/like_bloc/like_cubit.dart
@@ -1,13 +1,15 @@
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../models/models.dart';
+import '../../services/services.dart';
 
 class LikeCubit extends Cubit<List<int>> {
+  final _hive =  HiveServices();
   LikeCubit() : super([]);
 
 //fetches liked model from the database
   void loadLikes() async {
-      // TODO: implement loading
+    final List<int> tempList =_hive.getHiveKeys();
     emit(tempList);
   }
 
@@ -16,7 +18,7 @@ class LikeCubit extends Cubit<List<int>> {
     final List<int> tempList = List.from(state);
     tempList.add(model.id);
     emit(tempList);
-       // TODO: implement like submit
+     await _hive.putHiveModel(model:model);
   }
 
 ///deletes the unliked model from the database
@@ -24,7 +26,7 @@ class LikeCubit extends Cubit<List<int>> {
     final List<int> tempList = List.from(state);
     tempList.remove(model.id);
     emit(tempList);
-   // TODO: implement unlike submit
+    await _hive.deleteHiveKey(model:model);
   }
 
 }
