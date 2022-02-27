2008531399aad462b215d0dd2f9559683219f1c1
ehsanansari <ansari.eh@gmail.com>
2022-02-27 15:23:34 -0600

    implemented Hive in the photo model

--- lib/gallery/models/photo_model.dart
+++ lib/gallery/models/photo_model.dart
@@ -1,6 +1,5 @@
-
 /*
-Json structure:
+API structure:
 [
   {
     "albumId": 1,
@@ -13,25 +12,34 @@ Json structure:
  */
 
 import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
+part 'photo_model.g.dart';
 
+@HiveType(typeId: 0)
 class PhotoModel extends Equatable {
+  @HiveField(0)
   final int id;
+  @HiveField(1)
   final int albumId;
+  @HiveField(2)
   final String title;
+  @HiveField(3)
   final String url;
+  @HiveField(4)
   final String thumbnailUrl;
 
-  const PhotoModel(
-      {required this.albumId,
-      required this.id,
-      required this.title,
-      required this.url,
-      required this.thumbnailUrl,});
+  const PhotoModel({
+    required this.albumId,
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
 
   factory PhotoModel.fromJson(Map<String, dynamic> json) {
     return PhotoModel(
         albumId: json["albumId"] ?? 0,
-        id:  json["id"] ?? 0,
+        id: json["id"] ?? 0,
         title: json["title"] ?? "",
         url: json["url"] ?? "",
         thumbnailUrl: json["thumbnailUrl"] ?? "");
