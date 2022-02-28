be2ec9a6d1e5e570a71c0ce8574516aaed74370c
ehsanansari <ansari.eh@gmail.com>
2022-02-27 19:27:20 -0600

    Gallery page (main window) for showing Photo_bloc states

---
+++ lib/gallery/screens/gallery.dart
@@ -0,0 +1,46 @@
+import "package:flutter/material.dart";
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import '../widgets/widgets.dart';
+import '../bloc/bloc.dart';
+
+class Gallery extends StatefulWidget {
+  const Gallery({Key? key}) : super(key: key);
+
+  @override
+  _GalleryState createState() => _GalleryState();
+}
+
+class _GalleryState extends State<Gallery> {
+  @override
+  void dispose() {
+    super.dispose();
+    Hive.close();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<PhotoBloc>().add(const PhotoEventRequest());
+    context.read<LikeCubit>().loadLikes();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+      body: BlocConsumer<PhotoBloc, PhotoState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return state.requestStatus == PhotoRequestStatus.initialized
+              ? const LoaderWidget()
+              : state.requestStatus == PhotoRequestStatus.suscess
+                  ? PhotoListView(
+                      models: state.photoList,
+                    )
+                  : ErrorDisplayWidget(errorMessage: state.errorMessage);
+        },
+      ),
+    );
+  }
+}
