6e6686c90c3fa376744769b1562ea8d4d1492d92
ehsanansari <ansari.eh@gmail.com>
2022-02-27 19:11:01 -0600

    implemented Hive methods

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import '../config/config.dart';
+import 'package:infinite_gallery/gallery/screens/gallery.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../gallery/bloc/bloc.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -6,10 +10,15 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        theme: ThemeData(
+          primaryColor: InfiniteColors.blue,
+          backgroundColor: InfiniteColors.lightBackground,
+          splashColor: InfiniteColors.transparent,
+            // primarySwatch: Colors.blue,
+            ),
+        home: MultiBlocProvider(providers: [
+          BlocProvider<PhotoBloc>(create: (context) => PhotoBloc()),
+          BlocProvider<LikeCubit>(create: (context) => LikeCubit()),
+        ], child: const Gallery()));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:infinite_gallery/app/app.dart';
+import 'package:infinite_gallery/app/bloc_observer.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infinite_gallery/config/config.dart';
+import 'package:infinite_gallery/gallery/models/models.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
-  runApp(const App());
+void main() async {
+  
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(PhotoModelAdapter());
+  await Hive.openBox<PhotoModel>(kHiveBox);
+
+  BlocOverrides.runZoned(() => runApp(const App()),
+      blocObserver: MyBlocObserver());
 }
-
-
