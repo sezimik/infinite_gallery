7d95a784cacffddd33ac8f7046ce7fb0d0bb25ca
ehsanansari <ansari.eh@gmail.com>
2022-02-27 22:52:37 -0600

    minor

--- lib/gallery/repositories/photo_repository.dart
+++ lib/gallery/repositories/photo_repository.dart
@@ -2,7 +2,6 @@ import 'package:http/http.dart' as http;
 import '../models/models.dart';
 import 'dart:convert';
 
-
 class PhotoRepository {
   static const baseUrl = "https://jsonplaceholder.typicode.com";
   static const resourceType = "photos";
@@ -11,22 +10,21 @@ class PhotoRepository {
   http.Client httpClient = http.Client();
 
   /// Dependency injection for tests
-  PhotoRepository({http.Client? client}){
+  PhotoRepository({http.Client? client}) {
     httpClient = client ?? http.Client();
   }
 
-
   Future<List<PhotoModel>> fetchPhotos({required int startIndex}) async {
-
     final String _photoQuertUrl =
         "$baseUrl/$resourceType?_start=$startIndex&_limit=$querylimit";
+        
     final _response = await httpClient.get(Uri.parse(_photoQuertUrl));
 
     if (_response.statusCode == 200) {
-      final json = jsonDecode(_response.body);
+      final data = json.decode(_response.body);
       List<PhotoModel> photosList =
-          (json as List).map((e) => PhotoModel.fromJson(e)).toList();
- 
+          (data as List).map((e) => PhotoModel.fromJson(e)).toList();
+
       return photosList;
     } else {
       throw Exception("Something went wrong!");
