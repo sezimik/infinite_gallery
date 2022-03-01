5c058d6d35b7095de0e79190e1d4b82e4f8786b9
ehsanansari <ansari.eh@gmail.com>
2022-02-28 21:01:14 -0600

    added a constant for bloc errors

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -4,3 +4,4 @@ const String kUnlikeTItle = "Unlike";
 const String kCloseTitle = "Close";
 const String kListWarning = "No more images!";
 const String kLoadingTitle = "Loading ...";
+const String kErrorMessage = "Somthing went wrong!\nplease check your internet connection.";
