6cd9529cb00e0e817c35921820b4b78d23aaa7da
ehsanansari <ansari.eh@gmail.com>
2022-02-28 21:02:31 -0600

    Created a barrel/export file for Gallery

---
+++ lib/gallery/gallery.dart
@@ -0,0 +1,6 @@
+export 'bloc/bloc.dart';
+export 'models/models.dart';
+export 'repositories/repositories.dart';
+export 'screens/screens.dart';
+export 'services/services.dart';
+export 'widgets/widgets.dart';
